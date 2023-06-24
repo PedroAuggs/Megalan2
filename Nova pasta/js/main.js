b8805d761cdb768cc3ee58c009fb3b521c75e16b
PedroAuggs <135389331+PedroAuggs@users.noreply.github.com>
2023-06-23 21:11:38 -0300

    Add files via upload

---
+++ Nova pasta/js/main.js
@@ -0,0 +1,21 @@
+window.sr = ScrollReveal({ reset: true })
+
+sr.reveal('.area-1, { duration: 1500}')
+
+
+
+
+$("nav a, .btn").on("click", function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $("html, body").animate(
+            {
+                scrollTop: $(hash).offset().top - 100
+            },
+            800
+        );
+    }
+});
